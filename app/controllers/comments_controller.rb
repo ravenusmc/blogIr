cfd06a3ba8365bf644f8bb80216108fcda2b672f
Mike Cuddy <mcuddy77@gmail.com>
2015-12-28 13:30:14 -0500

    after some struggle, comments section should be working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,10 +40,10 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
-  def update
+ def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to blog_post_path(id: @comment.blog_post_id), notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to blog_post_path(id: @comment.blog_post_id), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
